f79346e5210c0414f2961f0058368015b0772360
Kanak <knk@gmail.com>
2026-04-05 15:16:05 +0530

    display realtime temp

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,25 @@
 const apiKey = "ddd19d9a47ccc9a29728517edf2b2a92";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".search-input");
+const icon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}&units=metric`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = data.main.temp + "&deg;C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  if (data.weather[0].main === "Clear") {
+    icon.innerHTMML.src = "images.rain.png";
+  }
+}
+
+btn.addEventListener("click", () => {
+  checkWeather(input.value);
+});
